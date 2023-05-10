6f82d3d9a685e1b741ae58565ba088059d375d25
mardMasoud <mardmasoud66@google.com>
2023-05-10 12:45:27 +0330

    final

--- js/script.js
+++ js/script.js
@@ -1,32 +1,21 @@
-const images = ["1.jpg", "2.png", "3.jpg"];
+const images = ["image/1.jpg", "image/2.png", "image/3.jpg"];
 let imgElm = document.querySelector("img");
 const prevElm = document.querySelector(".prev");
+const nextElm = document.querySelector(".next");
 let count = 0;
-// setInterval(() => {imgElm.classList.remove("animate__fadeInRight")
-//   count++;
-//   if (count < 3) {
-//     imgElm.setAttribute("src", "image/" + images[count]);
-  
-// }
+function prevImage() {
+  count--;
+  if (count < 0) count = images.length-1;
+  imgElm.setAttribute('src',images[count])
+}
 
-//   if (count > 2) {
-//     count = 0;
-//     imgElm.setAttribute("src", "image/" + images[count]);
-//   }
+function nextImage() {
+  count++;
+  if (count > images.length - 1) count = 0;
+  imgElm.setAttribute('src',images[count])
+}
 
-//   console.log("---" + count);
-// }, 3000);
-prevElm.addEventListener("click", function () {
-  console.log(count);
-  if (count == 0) {
-    imgElm.setAttribute("src", "image/" + images[2]);
-    count = 2;
-    imgElm.classList.add("tr")
-    
-  } else {
-    count--;
-    imgElm.setAttribute("src", "image/" + images[count]);
-    imgElm.classList.add("tr")
-    
-  }
-});
+prevElm.addEventListener("click", prevImage);
+nextElm.addEventListener("click", nextImage);
+
+setInterval(nextImage,3000);
